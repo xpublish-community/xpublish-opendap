410732ef9e00c931e0c515ec750cbcd70f0ccb6e
Roland Schweitzer <roland.schweitzer@noaa.gov>
2023-12-12 14:53:35 -0800

    Update tests/server.py
    Separate quote and casts tests.
    
    Co-authored-by: Alex Kerney <abk@mac.com>

--- tests/server.py
+++ tests/server.py
@@ -9,11 +9,12 @@ ds = xarray.tutorial.open_dataset("air_temperature")
 
 ds_attrs_quote = xarray.tutorial.open_dataset("air_temperature")
 ds_attrs_quote.attrs["quotes"] = 'This attribute uses "quotes"'
-ds_attrs_quote.attrs["npint"] = np.int16(16)
-ds_attrs_quote.attrs["npintthirtytwo"] = np.int32(32)
+ds_attrs_cast = xarray.tutorial.open_dataset("air_temperature")
+ds_attrs_cast.attrs["npint"] = np.int16(16)
+ds_attrs_cast.attrs["npintthirtytwo"] = np.int32(32)
 
 rest = xpublish.Rest(
-    {"air": ds, "attrs_quote_types": ds_attrs_quote},
+    {"air": ds, "attrs_quote_types": ds_attrs_quote, "attrs_cast": ds_attrs_cast},
     plugins={"opendap": OpenDapPlugin()},
 )
 
