2f9d5b7112f466b2aace95afa355aea1aaecc79a
Alex Kerney <abk@mac.com>
2024-05-17 12:20:29 -0400

    Disable live server tests on Windows for now

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,16 +1,32 @@
-"""Test OpenDAP clients against Xpublish OpenDAP plugin."""
+"""Test OpenDAP clients against Xpublish OpenDAP plugin.
+
+Live tests are currently failing on Windows, see:
+- https://github.com/Unidata/netcdf-c/issues/2459
+- https://github.com/Unidata/netcdf4-python/issues/1246
+- https://github.com/pydata/xarray/issues/7773
+"""
+
+import sys
 
 import netCDF4
 import pytest
 import xarray as xr
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="NetCDF4 is failing on Windows Github Actions workers",
+)
 def test_netcdf4(xpublish_server):
     """Test opening OpenDAP air dataset directly with NetCDF4 library."""
     url = f"{xpublish_server}/datasets/air/opendap"
     netCDF4.Dataset(url)
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="NetCDF4 is failing on Windows Github Actions workers",
+)
 def test_default_xarray_engine(xpublish_server, dataset):
     """Test opening OpenDAP air dataset with default Xarray engine."""
     url = f"{xpublish_server}/datasets/air/opendap"
@@ -18,6 +34,10 @@ def test_default_xarray_engine(xpublish_server, dataset):
     assert ds == dataset
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="NetCDF4 is failing on Windows Github Actions workers",
+)
 @pytest.mark.parametrize(
     "engine",
     [
@@ -33,6 +53,10 @@ def test_xarray_engines(xpublish_server, engine, dataset):
     assert ds == dataset
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="NetCDF4 is failing on Windows Github Actions workers",
+)
 def test_attrs_quotes(xpublish_server):
     """Test that we are formatting OpenDAP attributes that contain '"' properly."""
     url = f"{xpublish_server}/datasets/attrs_quote/opendap"
@@ -41,6 +65,10 @@ def test_attrs_quotes(xpublish_server):
     assert ds.attrs["quotes"] == 'This attribute uses "quotes"'
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="NetCDF4 is failing on Windows Github Actions workers",
+)
 def test_attrs_types(xpublish_server):
     """Test that we are formatting OpenDAP attributes that contain '"' properly."""
     url = f"{xpublish_server}/datasets/attrs_cast/opendap"
